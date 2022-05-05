761f7998dd4987cfb1031090755a7c342af5c242
Luca Yan <lucayan0506>
2022-05-05 20:25:54 +0100

    merge word clould & index

---
+++ cloud.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => update_word_cloud())
+window.addEventListener('resize', () => update_word_cloud())
+
+function update_word_cloud(){
+    //clear word cloud
+    document.getElementById("word-cloud").innerHTML = "";
+
+    var percents = 30;
+    var parentWidth = parseInt(document.getElementById("word-cloud").clientWidth);
+    var pixels = parentWidth*(percents/100);
+    
+    console.log(pixels);
+    const texts = [
+        'Visual Basic', 'C#', 'C++', 'Django', 'JavaScript', 'HTML', 'CSS', 'Git', 'Python', 
+        'LeetCode', 'DFS', 'BFS', 'Dynamic Programming', 'Hash Table', 'Bynary Search', 'Bynary Tree'
+    ];
+    const option = {
+        'radius': pixels
+    };
+
+    //generate new one
+    TagCloud(document.querySelector('#word-cloud'), texts,option);
+}
